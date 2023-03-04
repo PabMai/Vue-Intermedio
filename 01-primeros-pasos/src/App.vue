c4c82bbc87c56e135951d2c5908dcc8149393583
Pablo <jpabmai@gmail.com>
2023-03-04 14:46:45 -0300

    Primeros pasos - Estructura de directorios recomendada

--- 01-primeros-pasos/src/App.vue
+++ 01-primeros-pasos/src/App.vue
@@ -1,19 +1,6 @@
-/* -------------------------------------------------------------------------- */
-/*                               Consideraciones                              */
-/*                                                                            */
-/* 1 - Al importar un archivo extrerno se debe sacar la palabra setup         */
-/* de la etiqueta script                                                      */
-/*                                                                            */
-/* 2 - En el archivo externo se debe usar la importación de defineComponent   */
-/* (usar composition API: setup)                                              */
-/* -------------------------------------------------------------------------- */
-
-<script lang="ts" src="./App.ts"></script>
-
 <template>
   <div class="wrapper">
-    <h1>Counter: {{ counter }}</h1>
-    <button @click="increase">+1</button>
+    <h1>App.js</h1>
   </div>
 </template>
 
