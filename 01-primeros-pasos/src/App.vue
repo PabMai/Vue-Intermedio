a7890ee9fea213dc68a209d10cb08feacfa1ce6d
Pablo <jpabmai@gmail.com>
2023-03-04 13:58:01 -0300

    Primeros pasos - Contador con versión Composition API: setup

--- 01-primeros-pasos/src/App.vue
+++ 01-primeros-pasos/src/App.vue
@@ -1,11 +1,32 @@
-<script setup lang="ts">
+<script lang="ts">
+
+import { defineComponent, ref } from 'vue';
+
+// Version Composition API: setup
+export default defineComponent({
+  setup() {
+    const counter = ref(0);
+
+    const increase = () => {
+      counter.value++;
+    }
+
+    return {
+      // Properties
+      counter,
+
+      // Methods
+      increase
+    }
+  }
+});
 
 </script>
 
 <template>
   <div class="wrapper">
-    <h1>Hola</h1>
-    <p>Mundo</p>
+    <h1>Counter: {{ counter }}</h1>
+    <button @click="increase">+1</button>
   </div>
 </template>
 
