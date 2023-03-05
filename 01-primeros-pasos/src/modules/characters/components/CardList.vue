a05ba9546c0ceb4f12476528a17a77f21668b4c8
Pablo <jpabmai@gmail.com>
2023-03-05 10:25:20 -0300

    Petición HTTP - Composable functions - useCharacters con estado global

--- 01-primeros-pasos/src/modules/characters/components/CardList.vue
+++ 01-primeros-pasos/src/modules/characters/components/CardList.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { useCharacters } from '../composables/useCharacters';
 
-const { characters, isLoading } = useCharacters();
+const { characters, isLoading, hasError, errorMessage } = useCharacters();
 
 </script>
 
 <template>
     <h1 v-if="isLoading">Loading...</h1>
+    <h1 v-if="hasError">{{ errorMessage }}</h1>
     <ul>
         <li v-for="character of characters" :key="character.id">{{ character.name }}</li>
     </ul>
--- 01-primeros-pasos/src/modules/characters/composables/useCharacters.ts
+++ 01-primeros-pasos/src/modules/characters/composables/useCharacters.ts
@@ -1,20 +1,46 @@
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import rickandmortyApi from '@/api/rickandmortyApi';
 import type { CharacterData, CharacterResult } from '@/modules/characters/interfaces/character';
 
-export const useCharacters = () => {
-  const characters = ref<CharacterResult[]>([]);
-  const isLoading = ref<boolean>(true);
+const characters = ref<CharacterResult[]>([]);
+const isLoading = ref<boolean>(true);
+const hasError = ref<boolean>(false);
+const errorMessage = ref<string>('');
 
-  rickandmortyApi.get<CharacterData[]>('/character').then((resp) => {
-    //characters.value
-    characters.value = resp.data.results;
-    isLoading.value = false;
+export const useCharacters = () => {
+  // Lifecycle Hooks
+  onMounted(async () => {
+    await loadCharacters();
   });
+  // Methods
+  const loadCharacters = async () => {
+    if (characters.value.length > 0) return;
+
+    try {
+      const data = await rickandmortyApi
+        .get<CharacterData[]>('/character')
+        .then((resp) => resp.data.results);
+
+      characters.value = data;
+      isLoading.value = false;
+    } catch (error) {
+      hasError.value = true;
+      errorMessage.value = JSON.stringify(error);
+    }
+  };
+
+  if (characters.value.length === 0) {
+    rickandmortyApi.get<CharacterData[]>('/character').then((resp) => {
+      characters.value = resp.data.results;
+      isLoading.value = false;
+    });
+  }
 
   return {
     // Properties
     characters,
-    isLoading
+    isLoading,
+    hasError,
+    errorMessage
   };
 };
