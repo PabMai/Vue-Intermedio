8f56ff6b50ecdf96c788107dce0575a792af906d
Pablo <jpabmai@gmail.com>
2023-03-05 10:36:21 -0300

    Petición HTTP -  Vue TanStack Query

--- 01-primeros-pasos/src/main.ts
+++ 01-primeros-pasos/src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import routerMain from './router';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
 // Styles
 import './assets/main.css';
 
 const app = createApp(App);
+app.use(VueQueryPlugin);
 app.use(routerMain);
 
 app.mount('#app');
