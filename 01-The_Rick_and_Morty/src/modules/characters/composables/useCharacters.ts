c38bfba2b78fbe54ae9d014a1b2a548fe438298f
Pablo <jpabmai@gmail.com>
2023-03-11 15:20:35 -0300

    Composables - useCharacters funcionando

--- 01-The_Rick_and_Morty/src/modules/characters/composables/useCharacters.ts
+++ 01-The_Rick_and_Morty/src/modules/characters/composables/useCharacters.ts
@@ -1,21 +1,51 @@
 import { ref } from 'vue';
 
 import type { CharacterResult } from '@/modules/characters/interfaces/character';
+import characterStore from '@/stores/characters.store';
+import rickandmortyApi from '@/api/rickandmortyApi';
+import { useQuery } from '@tanstack/vue-query';
 
 const characters = ref<CharacterResult[]>([]);
 const isLoading = ref<boolean>(false);
 const isError = ref<boolean>(false);
 const errorMessage = ref<string | null>(null);
 
+const getAllCharacters = async () => {
+	if (characters.value.length > 0) {
+		return characters.value;
+	}
+
+	const { data } = await rickandmortyApi.get<CharacterData[]>('/character');
+
+	characterStore.loadedCharacters(data.results);
+
+	return data.results;
+};
+
+const loadedCharacters = (data: CharacterResult[]) => {
+	isError.value = false;
+	errorMessage.value = null;
+	characters.value = data;
+};
+
 const useCharacters = () => {
+	useQuery(['characters'], getAllCharacters, {
+		// onSuccess(data) {
+		// 	loadedCharacters(data);
+		// }
+		// Es igual a:
+		onSuccess: loadedCharacters
+	});
+
 	return {
 		// Properties
 		characters,
 		errorMessage,
 		isError,
-		isLoading
+		isLoading,
 
 		// Getters
+		count: characters.value.length
 
 		// Methods
 	};
--- 01-The_Rick_and_Morty/src/modules/characters/pages/CharacterListPage.vue
+++ 01-The_Rick_and_Morty/src/modules/characters/pages/CharacterListPage.vue
@@ -1,51 +1,25 @@
 <script setup lang="ts">
 
-import characterStore from '@/stores/characters.store';
-import rickandmortyApi from '@/api/rickandmortyApi';
 import CardList from '@/modules/characters/components/CardList.vue';
-import { useQuery } from '@tanstack/vue-query';
+import useCharacters from '@/modules/characters/composables/useCharacters';
 
 const props = defineProps<{ title: string, visible: boolean }>();
 
-const getAllCharactersCache = async () => {
-
-    if (characterStore.characters.count > 0) {
-        return characterStore.characters.list;
-    }
-
-    const { data } = await rickandmortyApi.get<CharacterData[]>('/character');
-
-    characterStore.loadedCharacters(data.results)
-
-    return data.results;
-}
-
-useQuery(
-    ['characters'],
-    getAllCharactersCache,
-    {
-        onSuccess(data) {
-            characterStore.loadedCharacters(data);
-        },
-        onError(error: any) {
-            characterStore.loadCharactersFailed(error.message)
-        }
-    }
-);
+const { characters, errorMessage, isError, isLoading, count } = useCharacters();
 
 </script>
 
 <template>
     <div>
-        <h1 v-if="characterStore.characters.isLoading">Loading...</h1>
-        <div v-if="characterStore.characters.isError">
+        <h1 v-if="isLoading">Loading...</h1>
+        <div v-if="isError">
             <h1>Error al cargar</h1>
-            <p>{{ characterStore.characters.errorMessage }}</p>
+            <p>{{ errorMessage }}</p>
         </div>
         <template v-else>
-            <h2>{{ props.title }}</h2>
+            <h2>{{ props.title }} - ({{ count }})</h2>
 
-            <CardList :characters="characterStore.characters.list" />
+            <CardList :characters="characters" />
         </template>
 
     </div>
