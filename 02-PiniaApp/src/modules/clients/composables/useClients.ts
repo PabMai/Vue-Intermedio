e322ac1359b3f38b2d18125bab49154f09849843
Pablo <jpabmai@gmail.com>
2023-03-19 13:54:25 -0300

    Pinia - Loading Modal

--- 02-PiniaApp/src/modules/clients/composables/useClients.ts
+++ 02-PiniaApp/src/modules/clients/composables/useClients.ts
@@ -6,6 +6,11 @@ import { storeToRefs } from 'pinia';
 import { watch, computed } from 'vue';
 
 const getClients = async (page: number): Promise<Client[]> => {
+
+  // await new Promise( resolve => {
+  //   setTimeout( () => resolve(true), 1500 );
+  // });
+
   const { data } = await clientsApi.get<Client[]>(`/clients?_page=${page}`);
 
   return data;
--- 02-PiniaApp/src/modules/clients/pages/ClientListPage.vue
+++ 02-PiniaApp/src/modules/clients/pages/ClientListPage.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import ClientList from '@/modules/clients/components/ClientList.vue';
 import PaginationNumber from '@/modules/clients/components/PaginationNumber.vue';
+import useClients from '@/modules/clients/composables/useClients';
+import LoadingModal from '@/modules/shared/components/LoadingModal.vue';
+
+const { isLoading } = useClients()
 
 </script>
 
@@ -12,6 +16,7 @@ import PaginationNumber from '@/modules/clients/components/PaginationNumber.vue'
     <!-- Pagination -->
     <PaginationNumber />
     <!-- Loading Modal -->
+    <LoadingModal v-if="isLoading" />
   </div>
 </template>
 
