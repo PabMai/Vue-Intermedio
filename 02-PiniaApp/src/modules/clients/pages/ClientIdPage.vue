a7fdb464897f619b9a9cfba7b7b93f841e1fd0a5
Pablo <jpabmai@gmail.com>
2023-03-26 21:41:12 -0300

    Mutaciones y queries - Continuación de la sección

--- 02-PiniaApp/src/modules/clients/pages/ClientIdPage.vue
+++ 02-PiniaApp/src/modules/clients/pages/ClientIdPage.vue
@@ -1,9 +1,41 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import LoadingModal from '@/modules/shared/components/LoadingModal.vue';
+
+</script>
 
 <template>
   <div>
-    <h1>Cliente por ID</h1>
+    <h3>Guardando...</h3>
+    <h3>Guardado</h3>
+
+    <LoadingModal v-if="false" />
+
+    <div>
+      <h1>Nombre</h1>
+      <form>
+        <input type="text" placeholder="Nombre del cliente">
+        <br>
+        <input type="text" placeholder="Dirección">
+        <br>
+        <button type="submit">Guardar</button>
+      </form>
+    </div>
+
+    <code>Toda la info del cliente</code>
+
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+input {
+  width: 100%;
+  padding: 5px 10px;
+  margin-bottom: 10px;
+}
+
+button {
+  margin-bottom: 10px;
+}
+
+</style>
