3373c27b024e84f7932f84499870452cf27a3ab5
Pablo <jpabmai@gmail.com>
2023-04-06 18:42:30 -0300

    Issues Quasar - Listado de issues: estructura

--- 03-issues-app/src/modules/issues/views/ListView.vue
+++ 03-issues-app/src/modules/issues/views/ListView.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import LoaderSpinner from '@modules/shared/components/LoaderSpinner.vue';
 
 </script>
 
 <template>
-  <div>
-    <span class="text-h2">ListPage</span>
+  <div class="row q-mb-md">
+    <div class="col-12">
+      <span class="text-h4">Github Issues</span>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-xs-12 col-md-4">
+      <!-- TODO: Filtros -->
+      <!-- FilterSelector -->
+    </div>
+    <div class="col-xs-12 col-md-8">
+      <!-- TODO: Loader -->
+      <LoaderSpinner color="white" />
+      <!-- TODO: IssueList - Array de IssueCard -->
+    </div>
   </div>
 </template>
 
