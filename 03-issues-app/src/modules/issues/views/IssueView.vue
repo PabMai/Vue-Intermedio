673bff24d88f8988d9990ba88df0f0c11eb0de04
Pablo <jpabmai@gmail.com>
2023-04-14 19:23:15 -0300

    Issues Quasar - Issue Page: Mostrar un issue de forma individual

--- 03-issues-app/src/modules/issues/views/IssueView.vue
+++ 03-issues-app/src/modules/issues/views/IssueView.vue
@@ -1,10 +1,23 @@
 <script setup lang="ts">
+import { useRoute } from 'vue-router';
+import LoaderSpinner from '@modules/shared/components/LoaderSpinner.vue';
+import IssueCard from '@modules/issues/components/issue-list/IssueCard.vue';
+
+const route = useRoute();
+const { id } = route.params;
 
 </script>
 
 <template>
-  <div>
-    <span class="text-h2">IssuePage: ID</span>
+  <LoaderSpinner color="white" />
+  <RouterLink class="text-white" to="/">Go Back</RouterLink>
+  <!-- Header -->
+  <IssueCard />
+  <!-- Comments -->
+  <LoaderSpinner :thickness="1" size="1.5rem" :show-text="false" />
+  <div class="column">
+    <span class="text-h3 q-mb-md">Comments (5)</span>
+    <IssueCard v-for="comment of 5" :key="comment" />
   </div>
 </template>
 
