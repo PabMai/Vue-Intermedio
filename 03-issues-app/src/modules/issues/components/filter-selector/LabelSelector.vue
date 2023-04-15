b7866fb145733b49769678a32ad95edc5495ab74
Pablo <jpabmai@gmail.com>
2023-04-14 22:46:38 -0300

    Issues Quasar - Obtener los labels de GitHub

--- 03-issues-app/src/modules/issues/components/filter-selector/LabelSelector.vue
+++ 03-issues-app/src/modules/issues/components/filter-selector/LabelSelector.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
 import LoaderSpinner from '@modules/shared/components/LoaderSpinner.vue';
+import useLabels from '@modules/issues/composables/useLabels';
+import { Label } from '../../interfaces/label';
+
+const { labelsQuery } = useLabels();
+console.log(labelsQuery.data)
+
+const data = labelsQuery.data;
 
 </script>
 
 <template>
-  <LoaderSpinner size="50px" :thickness="1" :show-text="false" />
-  <q-chip v-for="label of 10" :key="label" color="primary" outline clickable>Nombre</q-chip>
+  <LoaderSpinner size="50px" :thickness="1" :show-text="false" v-if="!labelsQuery.isLoading" />
+  <q-chip v-for="label of data" :key="label.id" color="primary" outline clickable>{{ label.name
+  }}</q-chip>
 </template>
 
 <style scoped></style>
