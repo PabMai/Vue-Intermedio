f92f1f58cda676558c8d3d1179198d4e7f2469a5
Pablo <jpabmai@gmail.com>
2023-06-20 14:43:12 -0300

    Issues Quasar (Mutaciones) - Botones flotantes

---
+++ 03-issues-app/src/modules/issues/components/FloatingButtons.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+  enum SizeEnum {
+    Small = 'sm',
+    Medium = 'md',
+    Large = 'lg',
+    ExtraLarge = 'xl',
+  }
+
+  interface FabButton {
+    icon: string;
+    color?: string;
+    size?: 'xs'|'sm'|'md'|'lg'|'xl';
+    action: () => void;
+  }
+
+  interface Props{
+    buttons: FabButton[]
+  }
+
+  const props = defineProps<Props>();
+</script>
+
+<template>
+  <div class="fab">
+		<q-btn class="q-mr-md"
+      v-for="(button, index) of props.buttons" round
+      :color="button.color || 'primary'"
+      :icon="button.icon"
+      :size="button.size || 'md'"
+      :key="index"
+      @click="button.action"
+    />
+	</div>
+</template>
+
+<style lang="css" scoped>
+.fab {
+	position: fixed;
+	bottom: 30px;
+	right: 30px;
+}
+</style>
--- 03-issues-app/src/modules/issues/views/ListView.vue
+++ 03-issues-app/src/modules/issues/views/ListView.vue
@@ -3,8 +3,17 @@ import LoaderSpinner from '@modules/shared/components/LoaderSpinner.vue';
 import FilterSelector from '@modules/issues/components/filter-selector/FilterSelector.vue';
 import IssueList from '@modules/issues/components/issue-list/IssueList.vue';
 import useIssues from '../composables/useIssues';
+import FloatingButtons from '../components/FloatingButtons.vue';
 
 const { issuesQuery } = useIssues();
+
+const listPageClickTemp1 = () => {
+  console.log('listPageClickTemp1');
+}
+
+const listPageClickTemp2 = () => {
+  console.log('listPageClickTemp2');
+}
 
 </script>
 
@@ -25,6 +34,22 @@ const { issuesQuery } = useIssues();
       <IssueList v-else :issues="issuesQuery.data?.value || []" />
     </div>
   </div>
+
+  <!-- FloatingButtons -->
+  <FloatingButtons :buttons="[
+       {
+      icon: 'navigation',
+      color: 'secondary',
+      size: 'md',
+      action: listPageClickTemp2
+    },
+    {
+      icon: 'add',
+      color: 'primary',
+      size: 'md',
+      action: listPageClickTemp1
+    },
+  ]" />
 </template>
 
 <style scoped></style>
